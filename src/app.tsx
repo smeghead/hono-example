1b0eaabf6f8878e044f47e628e6cf156bbfed201
smeghead <smeghead7@gmail.com>
2025-05-09 22:27:19 +0900

    use hono Suspence

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,26 @@
 import { Context, Hono } from 'hono'
+import { renderToReadableStream, Suspense } from 'hono/jsx/streaming'
 
 export const app = new Hono()
 
+const AsyncComponent = async () => {
+  await new Promise((r) => setTimeout(r, 1000)) // sleep 1s
+  return <div>Done!</div>
+}
+
 app.get('/', (c: Context) => {
-    return c.html(
+    const stream = renderToReadableStream(
       <div>
         <h1>Hello</h1>
-        {c.req.url}
+        <Suspense fallback={<div>loading...</div>}>
+          <AsyncComponent />
+        </Suspense>
       </div>
     )
+    return c.body(stream, {
+      headers: {
+        'Transfer-Encoding': 'chunked',
+      }
+    })
   }
 )
