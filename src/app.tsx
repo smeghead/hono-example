20f9ee913f82ff92527df5f5840b77555d70ac5d
smeghead <smeghead7@gmail.com>
2025-05-11 13:15:21 +0900

    use serveStatic

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
-import { Context, Hono } from 'hono'
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/deno'
 
 // Todo storage definitions
 interface Todo { id: number; title: string; completed: boolean }
@@ -7,59 +8,7 @@ let nextId = 1
 
 export const app = new Hono()
 
-// Root handler for Todo HTML page
-app.get('/', (c: Context) => {
-  const html = `<!DOCTYPE html>
-<html lang="en">
-<head><meta charset="UTF-8"><title>Todo App</title></head>
-<body>
-<h1>Todo App</h1>
-<input id="newTodo" placeholder="New todo"><button onclick="addTodo()">Add</button>
-<div id="todos"></div>
-<script>
-  async function getTodos() {
-    const res = await fetch('/todos');
-    const todos = await res.json();
-    const container = document.getElementById('todos');
-    container.innerHTML = '';
-    todos.forEach(todo => {
-      const div = document.createElement('div');
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.checked = todo.completed;
-      checkbox.onchange = () => toggle(todo.id, checkbox.checked);
-      const span = document.createElement('span');
-      span.textContent = todo.title;
-      span.onclick = () => editTitle(todo.id, span);
-      div.appendChild(checkbox);
-      div.appendChild(span);
-      container.appendChild(div);
-    });
-  }
-  async function addTodo() {
-    const input = document.getElementById('newTodo');
-    const title = input.value;
-    if (!title) return;
-    await fetch('/todos', { method: 'POST', headers: {'Content-Type':'application/json'}, body: JSON.stringify({ title }) });
-    input.value = '';
-    getTodos();
-  }
-  async function toggle(id, completed) {
-    await fetch('/todos/' + id, { method: 'PUT', headers: {'Content-Type':'application/json'}, body: JSON.stringify({ completed }) });
-    getTodos();
-  }
-  function editTitle(id, span) {
-    const newTitle = prompt('New title', span.innerText);
-    if (newTitle != null) {
-      fetch('/todos/' + id, { method: 'PUT', headers: {'Content-Type':'application/json'}, body: JSON.stringify({ title: newTitle }) }).then(getTodos);
-    }
-  }
-  window.onload = getTodos;
-</script>
-</body>
-</html>`
-  return c.body(html, { headers: { 'content-type': 'text/html;charset=UTF-8' } })
-})
+app.use('/', serveStatic({ root: './static/' }))
 
 // Todos routes
 app.get('/todos', (c) => {
